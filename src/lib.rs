804baa160d51309cd3d2d721284b8c1ea6b54b41
Ingrid <git@ingrids.email>
2023-01-26 00:29:30 +0100

    implement adding nodes to the dag

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,30 @@
 use std::collections::BTreeSet;
 use std::rc::Rc;
 
-pub struct Dag<T> {
+pub struct Dag<T: Ord> {
     roots: BTreeSet<Rc<Node<T>>>,
 }
 
-struct Node<T> {
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub struct Node<T: Ord> {
     elem: T,
     children: BTreeSet<Rc<Node<T>>>,
 }
 
-impl<T> Dag<T> {
+impl<T: Ord> Dag<T> {
     pub fn new() -> Self {
         Dag {
             roots: BTreeSet::new(),
         }
+    }
+
+    pub fn add_node(&mut self, elem: T) -> Rc<Node<T>> {
+        let node = Rc::new(Node {
+            elem,
+            children: BTreeSet::new(),
+        });
+        self.roots.insert(node.clone());
+        node
     }
 }
 
