345d8e3394e506e9e30a361962cb3ee20da62add
Ingrid <git@ingrids.email>
2023-02-13 15:41:01 +0100

    make the dag a vector-backed data structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,23 @@
-use std::cell::RefCell;
 use std::collections::BTreeSet;
-use std::rc::Rc;
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-pub struct Node<T: Ord> {
-    elem: T,
-    children: BTreeSet<Rc<Link<T>>>,
+pub struct Node<T> {
+    pub elem: T,
+    pub children: BTreeSet<NodeId>,
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-pub struct Link<T: Ord>(RefCell<Node<T>>);
+type NodeId = usize;
 
 pub struct Dag<T: Ord> {
-    roots: BTreeSet<Rc<Link<T>>>,
+    pub roots: BTreeSet<NodeId>,
+    pub nodes: Vec<Node<T>>,
 }
 
-impl<T: Ord> Link<T> {
-    fn new(elem: T) -> Self {
-        Link(RefCell::new(Node {
+impl<T: Ord> Node<T> {
+    pub fn new(elem: T) -> Self {
+        Node {
             elem,
             children: BTreeSet::new(),
-        }))
-    }
-
-    fn add_child(&self, child: Rc<Link<T>>) {
-        self.0.borrow_mut().children.insert(child);
-    }
-
-    fn remove_child(&self, child: Rc<Link<T>>) {
-        self.0.borrow_mut().children.remove(&child);
+        }
     }
 }
 
@@ -36,43 +25,49 @@ impl<T: Ord> Dag<T> {
     pub fn new() -> Self {
         Dag {
             roots: BTreeSet::new(),
+            nodes: Vec::new(),
         }
     }
 
-    pub fn add_node(&mut self, elem: T) -> Rc<Link<T>> {
-        let link = Rc::new(Link::new(elem));
-        self.roots.insert(link.clone());
-        link
+    pub fn add_node(&mut self, elem: T) -> NodeId {
+        let index = self.nodes.len();
+        self.nodes.push(Node::new(elem));
+
+        self.roots.insert(index);
+
+        index
     }
 
-    pub fn add_edge(&mut self, parent: Rc<Link<T>>, child: Rc<Link<T>>) {
-        parent.as_ref().add_child(child.clone());
+    pub fn add_edge(&mut self, parent: NodeId, child: NodeId) {
+        // TODO: we can do better than unwrapping here
+        self.nodes.get_mut(parent).unwrap().children.insert(child);
 
         self.roots.remove(&child);
     }
 
-    pub fn add_node_with_children(&mut self, elem: T, children: Vec<Rc<Link<T>>>) {
+    pub fn add_node_with_children(&mut self, elem: T, children: Vec<NodeId>) {
         let new_node = self.add_node(elem);
 
         for child in children.into_iter() {
-            self.add_edge(new_node.clone(), child)
+            self.add_edge(new_node, child)
         }
     }
 
     // NOTE: this doesn't add to roots when a node no longer has parents,
     // only for use in transitive reduce.
-    fn remove_edge(parent: Rc<Link<T>>, child: Rc<Link<T>>) {
-        parent.as_ref().remove_child(child);
+    fn remove_edge(&mut self, parent: NodeId, child: NodeId) {
+        // TODO: we can do better than unwrapping here
+        self.nodes.get_mut(parent).unwrap().children.remove(&child);
     }
 
-    fn count_edges_iter(curr_node: Rc<Link<T>>, nodes_visited: &mut BTreeSet<Rc<Link<T>>>) -> u32 {
+    fn count_edges_iter(&self, curr_node: NodeId, nodes_visited: &mut BTreeSet<NodeId>) -> u32 {
         let mut edge_count = 0;
 
-        for child in curr_node.0.borrow().children.iter() {
+        for child in self.nodes.get(curr_node).unwrap().children.iter() {
             edge_count += 1;
 
             if !nodes_visited.contains(child) {
-                edge_count += Self::count_edges_iter(child.clone(), nodes_visited);
+                edge_count += self.count_edges_iter(*child, nodes_visited);
             }
         }
 
@@ -83,52 +78,52 @@ impl<T: Ord> Dag<T> {
 
     pub fn count_edges(&self) -> u32 {
         let mut edge_count = 0;
-        let mut nodes_visited: BTreeSet<Rc<Link<T>>> = BTreeSet::new();
+        let mut nodes_visited: BTreeSet<NodeId> = BTreeSet::new();
 
         for root in self.roots.iter() {
-            edge_count += Self::count_edges_iter(root.clone(), &mut nodes_visited);
+            edge_count += self.count_edges_iter(root.clone(), &mut nodes_visited);
         }
 
         edge_count
     }
 
-    fn recursive_parent_remove(parent: Rc<Link<T>>, child: Rc<Link<T>>) {
-        Self::remove_edge(parent.clone(), child.clone());
-        for granchild in child.0.borrow().children.iter() {
-            Self::recursive_parent_remove(parent.clone(), granchild.clone());
+    fn recursive_parent_remove(&mut self, parent: NodeId, child: NodeId) {
+        self.remove_edge(parent, child);
+        for granchild in self.nodes.get(child).unwrap().children.clone().iter() {
+            self.recursive_parent_remove(parent, *granchild);
         }
     }
 
-    fn transitive_reduce_iter(curr_node: Rc<Link<T>>) {
-        let children = curr_node.0.borrow().children.clone(); // FIXME: would be nice to not have to clone here
+    fn transitive_reduce_iter(&mut self, curr_node: NodeId) {
+        let children = self.nodes.get(curr_node).unwrap().children.clone(); // FIXME: would be nice to not have to clone here
 
         for child in children.iter() {
-            for granchild in child.0.borrow().children.iter() {
-                Self::recursive_parent_remove(curr_node.clone(), granchild.clone());
+            for granchild in self.nodes.get(*child).unwrap().children.clone().iter() {
+                self.recursive_parent_remove(curr_node, *granchild);
             }
         }
 
         for child in children.iter() {
-            Self::transitive_reduce_iter(child.clone());
+            self.transitive_reduce_iter(*child);
         }
     }
 
     // TODO: see if we can reduce the amount of rc cloning happening
-    pub fn transitive_reduce(&self) {
-        for root in self.roots.iter() {
-            Self::transitive_reduce_iter(root.clone())
+    pub fn transitive_reduce(&mut self) {
+        for root in self.roots.clone().iter() {
+            self.transitive_reduce_iter(root.clone())
         }
     }
 
-    fn cycle_check_iter(curr_node: Rc<Link<T>>, ancestors: &mut Vec<Rc<Link<T>>>) -> bool {
+    fn cycle_check_iter(&self, curr_node: NodeId, ancestors: &mut Vec<NodeId>) -> bool {
         if ancestors.contains(&curr_node) {
             return true;
         }
 
         ancestors.push(curr_node.clone());
 
-        for child in curr_node.0.borrow().children.iter() {
-            if Self::cycle_check_iter(child.clone(), ancestors) {
+        for child in self.nodes.get(curr_node).unwrap().children.iter() {
+            if self.cycle_check_iter(*child, ancestors) {
                 return true;
             }
         }
@@ -139,10 +134,10 @@ impl<T: Ord> Dag<T> {
     }
 
     pub fn cycle_check(&self) -> bool {
-        let mut ancestors: Vec<Rc<Link<T>>> = Vec::new();
+        let mut ancestors: Vec<NodeId> = Vec::new();
 
         for root in self.roots.iter() {
-            if Self::cycle_check_iter(root.clone(), &mut ancestors) {
+            if self.cycle_check_iter(*root, &mut ancestors) {
                 return true;
             }
         }
@@ -171,27 +166,27 @@ mod tests {
         let node4 = dag.add_node(4);
         let node5 = dag.add_node(5);
 
-        dag.add_edge(node1.clone(), node2.clone());
-        dag.add_edge(node1.clone(), node3.clone());
-        dag.add_edge(node1.clone(), node4.clone());
-        dag.add_edge(node1.clone(), node5.clone());
+        dag.add_edge(node1, node2);
+        dag.add_edge(node1, node3);
+        dag.add_edge(node1, node4);
+        dag.add_edge(node1, node5);
 
-        dag.add_edge(node2.clone(), node4.clone());
-        dag.add_edge(node3.clone(), node4.clone());
-        dag.add_edge(node3.clone(), node5.clone());
-        dag.add_edge(node4.clone(), node5.clone());
+        dag.add_edge(node2, node4);
+        dag.add_edge(node3, node4);
+        dag.add_edge(node3, node5);
+        dag.add_edge(node4, node5);
 
         assert_eq!(dag.count_edges(), 8);
-        assert!(node1.0.borrow().children.contains(&node4));
-        assert!(node1.0.borrow().children.contains(&node5));
-        assert!(node3.0.borrow().children.contains(&node5));
+        assert!(dag.nodes.get(node1).unwrap().children.contains(&node4));
+        assert!(dag.nodes.get(node1).unwrap().children.contains(&node5));
+        assert!(dag.nodes.get(node3).unwrap().children.contains(&node5));
 
         dag.transitive_reduce();
 
         assert_eq!(dag.count_edges(), 5);
-        assert!(!node1.0.borrow().children.contains(&node4));
-        assert!(!node1.0.borrow().children.contains(&node5));
-        assert!(!node3.0.borrow().children.contains(&node5));
+        assert!(!dag.nodes.get(node1).unwrap().children.contains(&node4));
+        assert!(!dag.nodes.get(node1).unwrap().children.contains(&node5));
+        assert!(!dag.nodes.get(node3).unwrap().children.contains(&node5));
     }
 
     #[test]
@@ -203,10 +198,10 @@ mod tests {
         let node3 = good_dag.add_node(3);
         let node4 = good_dag.add_node(4);
 
-        good_dag.add_edge(node1.clone(), node2.clone());
-        good_dag.add_edge(node1.clone(), node3.clone());
-        good_dag.add_edge(node2.clone(), node4.clone());
-        good_dag.add_edge(node3.clone(), node4.clone());
+        good_dag.add_edge(node1, node2);
+        good_dag.add_edge(node1, node3);
+        good_dag.add_edge(node2, node4);
+        good_dag.add_edge(node3, node4);
 
         assert!(!good_dag.cycle_check());
 
@@ -217,11 +212,11 @@ mod tests {
         let node3 = bad_dag.add_node(3);
         let node4 = bad_dag.add_node(4);
 
-        bad_dag.add_edge(node1.clone(), node2.clone());
-        bad_dag.add_edge(node1.clone(), node3.clone());
-        bad_dag.add_edge(node2.clone(), node4.clone());
-        bad_dag.add_edge(node4.clone(), node3.clone());
-        bad_dag.add_edge(node3.clone(), node2.clone());
+        bad_dag.add_edge(node1, node2);
+        bad_dag.add_edge(node1, node3);
+        bad_dag.add_edge(node2, node4);
+        bad_dag.add_edge(node4, node3);
+        bad_dag.add_edge(node3, node2);
 
         assert!(bad_dag.cycle_check());
     }
